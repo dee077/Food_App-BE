8d08b8d34598c28795b2f9a4e8c44f4db652bf14
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-10-27 18:17:26 +0530

    origin fix

--- server.js
+++ server.js
@@ -15,19 +15,13 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const cors = require('cors');
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
-app.use(
-  cors({
-    origin: ["https://www.hireanything.com", "https://hireanything.com"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
-
-// Handle preflight requests
 app.options('*', cors());
-
 
 async function connectDB() {
   try {
